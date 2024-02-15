0a7b9f64c5318a670120aea74bda1f2644f2196d
jlarson023 <larsontjonathan@gmail.com>
2024-02-15 10:29:28 -0800

    AnimatonOnClick Added
    Made explosion and mole prefabs

--- Assets/Jon'sFolder/MoleScript/BasicMoleScript.cs
+++ Assets/Jon'sFolder/MoleScript/BasicMoleScript.cs
@@ -5,14 +5,24 @@ using UnityEngine;
 public class BasicMoleScript : Clickable
 {
     public bool isHurt = false;
-    
+    public Animator moleAnim;
+    public GameObject explosion;
+
+    protected override void Start()
+    {
+        base.Start();
+        moleAnim = GetComponent<Animator>();
+    }
+
     protected override void GotClicked()
     {
         if (!isHurt)
         {
             base.GotClicked();
-            //play animation
-            Destroy(gameObject, 3);
+            moleAnim.SetBool("isHurt", isHurt);
+            GameObject thisExplosion =  Instantiate(explosion, transform.position, explosion.transform.rotation);
+            Destroy(gameObject, 1);
+            Destroy(thisExplosion, 1.1f);
             isHurt = true;
         }
     }
